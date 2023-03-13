e9ee5a6850f45b5786ff848a90995ee1c4925bcb
TheoBrunetti <theobrunettipro@gmail.com>
2023-03-13 18:54:04 +0100

    add hidden scroll in meal-card-instructions

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,9 @@ const Card = ({ meal }) => {
             <h2>{meal.strMeal}</h2>
             <p>Origine : {meal.strArea}</p>
             <img src={meal.strMealThumb} alt={"photo de " + meal.strMeal} />
-            <p>{meal.strInstructions}</p>
             <p>Type de plat : {meal.strTags}</p>
             <p>Catégorie : {meal.strCategory}</p>
+            <p className="meal-card-instructions">{meal.strInstructions}</p>
         </div>
     );
 };
